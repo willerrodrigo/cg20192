20e714df7a8f2272699bcea50ef12bdb5b5bff9d
willersilva <willerrodrigo2006@hotmail.com>
2019-08-07 20:45:23 -0300

    Implement load triangle mesh object

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,108 @@
-#include <iostream>
-
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
 
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
+
 #include <glm/vec3.hpp>
 #include <glm/mat3x3.hpp>
 
+#include <iostream>
 #include <vector>
+#include <stdio.h>
+#include <string>
+#include <cstring>
 
 // Implement load triangle mesh
 void loadTriangleMesh(
-					const std::string & filename,
-					std::vector<glm::vec3> & positions,
-					std::vector<glm::vec3> & normals,
-					std::vector<glm::vec2> & textureCoordinates,
-					std::vector<size_t> & positionIndices,
-					std::vector<size_t> & normalsIndices,
-					std::vector<size_t> & textureCoordinates){
+		const char * path, 
+		std::vector<glm::vec3> & out_vertices, 
+		std::vector<glm::vec2> & out_uvs,
+		std::vector<glm::vec3> & out_normals){
+
+		printf("Loading OBJ file %s...\n", path);
+
+		std::vector<unsigned int> vertexIndices, uvIndices, normalIndices;
+		std::vector<glm::vec3> temp_vertices; 
+		std::vector<glm::vec2> temp_uvs;
+		std::vector<glm::vec3> temp_normals;
+
+
+		FILE * file = fopen(path, "r");
+		if( file == NULL ){
+			printf("Impossible to open the file ! Are you in the right path ?\n");
+			getchar();
+		}
+
+		while( 1 ){
 	
-}
+			char lineHeader[128];
+			// read the first word of the line
+			int res = fscanf(file, "%s", lineHeader);
+			if (res == EOF)
+				break; // EOF = End Of File. Quit the loop.
+	
+			// else : parse lineHeader
+			
+			if ( strcmp( lineHeader, "v" ) == 0 ){
+				glm::vec3 vertex;
+				fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z );
+				temp_vertices.push_back(vertex);
+			}else if ( strcmp( lineHeader, "vt" ) == 0 ){
+				glm::vec2 uv;
+				fscanf(file, "%f %f\n", &uv.x, &uv.y );
+				uv.y = -uv.y; // Invert V coordinate since we will only use DDS texture, which are inverted. Remove if you want to use TGA or BMP loaders.
+				temp_uvs.push_back(uv);
+			}else if ( strcmp( lineHeader, "vn" ) == 0 ){
+				glm::vec3 normal;
+				fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z );
+				temp_normals.push_back(normal);
+			}else if ( strcmp( lineHeader, "f" ) == 0 ){
+				std::string vertex1, vertex2, vertex3;
+				unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+				int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2] );
+				if (matches != 9){
+					printf("File can't be read by our simple parser :-( Try exporting with other options\n");
+					fclose(file);
+				}
+				vertexIndices.push_back(vertexIndex[0]);
+				vertexIndices.push_back(vertexIndex[1]);
+				vertexIndices.push_back(vertexIndex[2]);
+				uvIndices    .push_back(uvIndex[0]);
+				uvIndices    .push_back(uvIndex[1]);
+				uvIndices    .push_back(uvIndex[2]);
+				normalIndices.push_back(normalIndex[0]);
+				normalIndices.push_back(normalIndex[1]);
+				normalIndices.push_back(normalIndex[2]);
+			}else{
+				// Probably a comment, eat up the rest of the line
+				char stupidBuffer[1000];
+				fgets(stupidBuffer, 1000, file);
+			}
+	
+		}
+	
+		// For each vertex of each triangle
+		for( unsigned int i=0; i<vertexIndices.size(); i++ ){
+	
+			// Get the indices of its attributes
+			unsigned int vertexIndex = vertexIndices[i];
+			unsigned int uvIndex = uvIndices[i];
+			unsigned int normalIndex = normalIndices[i];
+			
+			// Get the attributes thanks to the index
+			glm::vec3 vertex = temp_vertices[ vertexIndex-1 ];
+			glm::vec2 uv = temp_uvs[ uvIndex-1 ];
+			glm::vec3 normal = temp_normals[ normalIndex-1 ];
+			
+			// Put the attributes in buffers
+			out_vertices.push_back(vertex);
+			out_uvs     .push_back(uv);
+			out_normals .push_back(normal);
+		
+		}
+		fclose(file);
+};
 
 bool BACKGROUND_STATE = false;
 
@@ -55,7 +137,7 @@ int main(int argc, char** argv) {
 		7, 8, 9); // third column
 	
 	// std::cout << glm::to_string(m) << std::endl;
-	std::cout << glm::to_string(m[0]) << std::endl;
+	// std::cout << glm::to_string(m[0]) << std::endl;
 	
 	/* Initialize the library */
     if (!glfwInit())
@@ -113,6 +195,11 @@ int main(int argc, char** argv) {
 
 	// Destroy window
 	glfwDestroyWindow(window);
+	
+	std::vector< glm::vec3 > vertices;
+	std::vector< glm::vec2 > uvs;
+	std::vector< glm::vec3 > normals;
+	loadTriangleMesh("../res/meshes/object.obj", vertices, uvs, normals);
 	
     glfwTerminate();
 	return 0;
